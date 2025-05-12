d7923700f6f9504e06a444a656c6089fa6b00a87
Alice00021 <alisa_barysheva03@mail.ru>
2025-05-12 19:07:01 +0300

    add book_service, but  need to add methods

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -1,1 +1,28 @@
 package service
+
+import (
+    "context"
+    "test_go/internal/entity"
+    "test_go/internal/repo"
+)
+
+type BookService interface {
+    CreateBook(ctx context.Context, title string, authorID uint) (*entity.Book, error)
+    
+}
+
+type bookService struct {
+    repo repo.BookRepository
+}
+
+func NewBookService(repo repo.BookRepository) BookService {
+    return &bookService{repo: repo}
+}
+
+func (s *bookService) CreateBook(ctx context.Context, title string, authorID uint) (*entity.Book, error) {
+    book := &entity.Book{Title: title, AuthorID: authorID}
+    if err := s.repo.Create(ctx, book); err != nil {
+        return nil, err
+    }
+    return book, nil
+}
