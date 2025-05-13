81691a4518bd9c043e97c67c45ddb1e5b5038547
Alice00021 <alisa_barysheva03@mail.ru>
2025-05-13 10:49:17 +0300

    initialize book service

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -8,7 +8,10 @@ import (
 
 type BookService interface {
     CreateBook(ctx context.Context, title string, authorID uint) (*entity.Book, error)
-    
+    UpdateBook(ctx context.Context, title string, authorID uint) (*entity.Book, error)
+    DeleteBook(ctx context.Context, id uint)   error
+    GetByIDBook(ctx context.Context, id uint) (*entity.Book, error)
+    GetAllBooks(ctx context.Context) ([]entity.Book, error) 
 }
 
 type bookService struct {
@@ -25,4 +28,36 @@ func (s *bookService) CreateBook(ctx context.Context, title string, authorID uin
         return nil, err
     }
     return book, nil
+}
+
+func (s *bookService) UpdateBook(ctx context.Context, title string, authorID uint) (*entity.Book, error) {
+    book := &entity.Book{Title: title, AuthorID: authorID}
+    if err := s.repo.Update(ctx, book); err != nil {
+        return nil, err
+    }
+    return book, nil
+}
+
+
+func (s *bookService) DeleteBook(ctx context.Context, id uint)  error {
+    if err := s.repo.Delete(ctx, id); err != nil {
+        return err
+    }
+    return  nil
+}
+
+func (s *bookService) GetByIDBook(ctx context.Context, id uint)  (*entity.Book, error) {
+    book, err := s.repo.GetByID(ctx, id)
+    if err != nil {
+        return nil, err
+    }
+    return  book, nil
+}
+
+func (s *bookService) GetAllBooks(ctx context.Context) ([]entity.Book, error){
+    books, err := s.repo.GetAll(ctx)
+    if err!=nil{
+        return nil, err
+    }
+    return books, nil
 }
