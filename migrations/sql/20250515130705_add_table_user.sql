69ae7b1a771687109606b620d939dfe20ee202aa
Alice00021 <alisa_barysheva03@mail.ru>
2025-05-15 16:36:05 +0300

    add table users to bd

---
+++ migrations/sql/20250515130705_add_table_user.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    female VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
