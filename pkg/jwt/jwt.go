1833d39e3dc30c3ed37ed6666b3d97c1f5a5c9ff
Alice00021 <alisa_barysheva03@mail.ru>
2025-05-19 12:32:15 +0300

    add refresh and acess token

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -2,26 +2,36 @@ package jwt
 
 import (
 	"time"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
 const (
-	SecretKey = "какой-то-интересный-секретный-ключ" 
-	TokenExpiration = 24 * time.Hour
+	SecretKey          = "какой-то-интересный-секретный-ключ"
+	AccessTokenExpire  = 15 * time.Minute
+	RefreshTokenExpire = 7 * 24 * time.Hour
+	TokenExpiration    = 24 * time.Hour
 )
 
 type Claims struct {
-	UserID uint `json:"user_id"`
+	UserID   uint   `json:"user_id"`
 	Username string `json:"username"`
 	jwt.RegisteredClaims
 }
 
-func GenerateToken(userID uint, username string) (string, error) {
+func GenerateAccessToken(userID uint, username string)(string, error){
+	return generateToken(userID, username, AccessTokenExpire)
+}
+
+func GenerateRefreshToken(userID uint, username string)(string, error){
+	return generateToken(userID, username, RefreshTokenExpire)
+}
+func generateToken(userID uint, username string, expire time.Duration) (string, error) {
 	claims := &Claims{
 		UserID:   userID,
 		Username: username,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TokenExpiration)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expire)),
 		},
 	}
 
