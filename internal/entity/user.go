bdf2499da410312ba7c03e3260df57f5fc4d40a0
Alice00021 <alisa_barysheva03@mail.ru>
2025-08-27 10:30:24 +0300

    add profile photo to user

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -10,4 +10,21 @@ type User struct {
 	Surname  string
 	Username string
 	Password string
+	FilePath *string
+}
+
+type UserInput struct {
+	Name     string `json:"name" validate:"required"`
+	Surname  string `json:"surname" validate:"required"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func NewUser(name string, surname string, username string, password string) *User {
+	return &User{
+		Name:     name,
+		Surname:  surname,
+		Username: username,
+		Password: password,
+	}
 }
